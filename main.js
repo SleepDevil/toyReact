aaf5433ae132688608baebf04ed5bf154e213ce6
SleepDevil <1752915704@qq.com>
2020-07-26 09:41:06 +0800

    day02

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
 import { ToyReact, Component } from "./ToyReact.js"
+window.Squares = {}
 
-class MyComponent extends Component{
-    render() {
-        return <div>
-            <span>hello</span>
-            <span>world</span>
-            <div>
-                {this.children}
-            </div>
-        </div>
-    }
+class Square extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			value: null
+		}
+	}
+	render() {
+		window.Squares[this.props["value"]] = this
+		return (
+			<button className="square" onClick={() => this.setState({ value: 'X' })}>
+				{this.state.value ? this.state.value : ""}
+			</button>
+		)
+	}
 }
 
-let a = <MyComponent name="a" id="sleepdevil">
-    <div>123</div>asdasd
-    <span></span>
-</MyComponent>
+class Board extends Component {
+	renderSquare(i) {
+		return <Square value={i}></Square>
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		)
+	}
+}
+
+let a = <Board name="a" id="sleepdevil"></Board >
 
 ToyReact.render(a, document.body)
